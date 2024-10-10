b20ece0c2b21745867562b750975fb42a5586d32
Александр Пичугин <aleksandrpicugin700@gmail.com>
2024-10-10 20:18:59 +0300

    OperatorPoint Done

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define DISTANCE
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +12,43 @@ namespace Encapsulation
 	{
 		static void Main(string[] args)
 		{
-			Point A=new Point();
+#if DISTANCE
+			Point A = new Point();
+			//Point B = new Point();
 			//Console.WriteLine($"X={A.GetX()}\tY={A.GetY()}");
 			//A.SetX(2000);
 			//A.SetY(3000);
 			//A.Print();
+
 			A.X = 22;
 			A.Y = 33;
 			A.Print();
+			/*B.X = 56;
+			B.Y = 74;
+			B.Print();*/
+
+			//Point B = new Point();
+
+			Point B = new Point(A);// CopyConstructor
+			B.Print();
+
+			Point C;   // Это не объект а ссылка на объект
+			C = new Point(B);// Объект можно создать только при помощи оператора 'new'
+			C.Print();
+
+#endif
+
+			Point A = new Point(2, 3);
+			Point B = new Point(7, 8);
+			Point C = new Point(A + B);
+			C.Print();
+			Point D= new Point(A-B);
+			D .Print();
+				for (Point i=new Point();i.X<10;i++ )
+			{
+				i.Print();
+			}
+
 		}
 	}
 }
